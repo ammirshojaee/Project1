f6ab231f6d2c5afa76d244308b37266832977483
Kimberley Epelle <80205441+KimberleyEpelle@users.noreply.github.com>
2021-05-01 20:46:56 +0200

    some commit

---
+++ DigiShop/src/main/java/hu/unideb/inf/controller/adminMainController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.inf.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author KIMBERLET EPELLE
+ */
+public class adminMainController {
+    
+    @FXML
+    void modCashiers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modprod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCashiers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewprod(ActionEvent event) {
+
+    }
+}
