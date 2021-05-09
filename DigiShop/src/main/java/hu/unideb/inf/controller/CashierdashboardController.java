b928deb3267e7b5d7f8630173df0885e47b7b345
ammirshojaee <41487440+ammirshojaee@users.noreply.github.com>
2021-05-09 19:17:04 +0200

    lastone

--- DigiShop/src/main/java/hu/unideb/inf/controller/CashierdashboardController.java
+++ DigiShop/src/main/java/hu/unideb/inf/controller/CashierdashboardController.java
@@ -3,15 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fxml;
+package hu.unideb.inf.controller;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
@@ -38,17 +41,20 @@ public class CashierdashboardController implements Initializable {
     @FXML
     private BarChart<String, Number> Chart;
     @FXML 
-    void handleButtonAction(ActionEvent event){
-      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Cashierproductspage.fxml"));
-      Parent root1 = (Parent) fxmlLoader.load();
-      Stage stage = new Stage();
-      stage.setScene(new Scene(root1));  
-      stage.show();
+    void handleButtonAction(ActionEvent event) throws IOException{
+     FXMLLoader loader = new FXMLLoader(LoginPageController.class.getResource("/fxml/Cashierproductspage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(loader.load());
+   
+        stage.setTitle("LoginPage");
+        stage.setScene(scene);
+        stage.show();
     }
-
+    
     /**
      * Initializes the controller class.
-     */
+     * @param url
+     * @param rb  */
    @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
